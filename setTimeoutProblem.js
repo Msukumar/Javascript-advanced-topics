163869c2f90f9999c7f9b8cf45f561055e674c34
MEDIDSU <sukumar.medidi@one.verizon.com>
2018-08-04 19:59:00 -0400

    settimeout problem

---
+++ setTimeoutProblem.js
@@ -0,0 +1,19 @@
+const displayTest1 = () =>{
+    for(let i =0; i< 10; i++){
+    setTimeout(() => {
+      console.log(i); // will o/p 10 for 10 times
+    }, 1000);
+    }
+}
+  
+
+const displayTest2 = () =>{
+    for(var i =0; i< 10; i++){
+    setTimeout(() => {
+      console.log(i); // will o/p  0 t0 9 
+    }, i, 1000);
+    }
+}
+
+displayTest1();
+displayTest2();
