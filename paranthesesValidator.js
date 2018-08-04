37f8b73459d3a35578432574b79b163d81174034
kmdrGroch <bartosz.leoniak@wp.pl>
2018-08-04 22:54:53 +0200

    better syntax

--- paranthesesValidator.js
+++ paranthesesValidator.js
@@ -1,24 +1,24 @@
-function ParaValidator(inputValue) {
-	var paranthisis = "[]{}()";
-  var bracePosition, stack = [];
-  for(var i = 0; i<= inputValue.length; i++){
-  	bracePosition = paranthisis.indexOf(inputValue[i]);
-    if(bracePosition === -1){
-    	continue;
+function paraValidator(inputValue) {
+    const paranthisis = "[]{}()",
+        stack = [];
+    let bracePosition;
+
+
+    for(let i = 0; i <= inputValue.length; i++) {
+        bracePosition = paranthisis.indexOf(inputValue[i]);
+        if(bracePosition === -1) {
+            continue;
+        }
+
+        if(bracePosition % 2 === 0) {
+            stack.push(bracePosition + 1);
+        } else if(stack.pop() !== bracePosition) {
+            return false;
+        }
     }
-    
-    if(bracePosition % 2 === 0){
-    	stack.push(bracePosition + 1);
-    }else {
-    	if(stack.pop() !== bracePosition){
-      	return false;
-      }
-    }
-  }
-  
-  return stack.length === 0;
-  
+
+    return stack.length === 0;
 }
 
-console.log(ParaValidator("{{([])}}"));
-console.log(ParaValidator("{{(}}"));
+console.log(paraValidator("{{([])}}"));
+console.log(paraValidator("{{(}}"));
