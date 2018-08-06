0459ad2448f1d7409935a9b61409c4085229c691
kmdrGroch <bartosz.leoniak@wp.pl>
2018-08-06 17:28:08 +0200

    Update add(2)(3)(5).spec.js

--- tests/add(2)(3)(5).spec.js
+++ tests/add(2)(3)(5).spec.js
@@ -2,6 +2,6 @@ const add = require("../src/add(2)(3)(5)");
 
 describe("add", () => {
     it("should sum up chain methods", () => {
-        expect(add(2)(3)(5)()).toBe(10);
+        expect(add(2)(3)(5).valueOf()).toBe(10);
     });
 });
