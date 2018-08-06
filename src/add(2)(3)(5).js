5915c10316e9da8ef76f401c8bfcb6a0e90e235c
kmdrGroch <bartosz.leoniak@wp.pl>
2018-08-06 17:27:18 +0200

    moved back in functionality

--- src/add(2)(3)(5).js
+++ src/add(2)(3)(5).js
@@ -1,8 +1,10 @@
 function sum(x) {
-    const add = y => isNaN(y) ? x : sum(x + y);
+    const add = y => sum(x + y);
+    add.valueOf = () => x;
     return add;
 }
 
-console.log(sum(2)(3)(5)()); // 10
+console.log(sum(2)(3)(5)); // f 10
+console.log(sum(2)(3)(5).valueOf()); // 10
 
 module.exports = sum;
